f28ee58b44dd2887e09421ec9f5fc07c64fcd2fd
Funduchka <kotikbryansk@gmail.com>
2021-10-19 22:34:43 +0300

    d added and some changes

--- main.cpp
+++ main.cpp
@@ -2,11 +2,17 @@
 
 using namespace std;
 
+///
+/// \brief main
+/// \return
+///
 int main()
 {
-    int a = 30;
+    int a = 60; // Мой эксперимент
     float b = 3.14f;
+    short d = 5;
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
+    cout << "d = " << d << endl;
     return 0;
 }

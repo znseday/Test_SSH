33e2d16c703e2ecb9fd75b51a5659e3113306f6e
Sergey Tsynikin <znseday@yandex.ru>
2021-10-19 22:11:53 +0300

    c added

--- main.cpp
+++ main.cpp
@@ -6,7 +6,9 @@ int main()
 {
     int a = 30;
     float b = 3.14f;
+    char c = 'A';
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
+    cout << "c = " << c << endl;
     return 0;
 }
